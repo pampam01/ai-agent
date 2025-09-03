6370d6f8d6a3c8aa54dbe4a09a86747abc08f734
pampam01 <emailhpwiko@gmail.com>
2025-09-04 00:05:33 +0700

    create test and add prompt

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+
+    if len(sys.argv) < 2:
+        print("Please provide a prompt")
+        return
+
+    prompt = sys.argv[1]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=
+        """
+        tulis cerita singkat tentang seorang pengusaha sukses
+        namanya adalah prayitno 
+        dia memiliki 2 saudara kembar yaitu dua sastra
+        mereka tinggal bersama ayah dan ibu mereka di desa sederhana
+        mereka memiliki 2 kakak yaitu budi dan toni
+        budi adalah seorang pengusaha sukses
+        """,
+
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None: 
+        raise ValueError("No response from model")
+
+    print("prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+
+
+print("args", sys.argv)
